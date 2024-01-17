3dc3e4c40c3e67057f2c3ffae5c7443655acb69f
Bebooop <metallibilly1991@yahoo.com>
2024-01-16 20:35:27 -0500

    added input checking and hello command

--- rolldice.py
+++ rolldice.py
@@ -1,14 +1,38 @@
 import random
 import sys
 
-if len(sys.argv) != 3:
+def printusage():
 	print("rolldice.py [dice count] [dice size]")
 	print("For usage with GURPS type in 'rolldice.py 3 6'")
+	print("Please use whole, positive numbers")
+
+if len(sys.argv) != 3:
+	printusage()
 	exit(1)
 
-dice_size = int(sys.argv[2])
-dice_count = int(sys.argv[1])
+if sys.argv[1] == "hello":
+	print("Hello!")
+	print("Please insert dice!")
+	exit(1)
+
+dice_size = 0
+dice_count = 0
 sum = 0
+
+try: 
+	dice_size = int(sys.argv[2])
+	dice_count = int(sys.argv[1])
+except:
+	printusage()
+	exit(1)
+
+if dice_size < 1:
+	printusage()
+	exit(1)
+
+if dice_count < 1:
+	printusage()
+	exit(1)
 
 for i in range(dice_count):
 	roll = random.randint(1, dice_size)
